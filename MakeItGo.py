ab555981b4e8750e7046f93988c6195df850ed85
Sokolov Lev Sergeevich <89748410+levs16@users.noreply.github.com>
2023-03-26 11:48:52 +0300

    More "cosmetic" changes to the apps' print-outs

--- MakeItGo.py
+++ MakeItGo.py
@@ -11,17 +11,17 @@ try: # Using the "try" to execute
         print(f"Starting...") # Print-out
 
         if os.path.isfile(obj): # Checking, whether the name is file, if yes, then proceed
-            print(f"\n---------Script--------\Removing {obj}..\n-------------------\n.")
+            print(f"\n---------Script--------\nRemoving {obj}..\n-------------------\n.")
             k = random.randint(9999, 999999) # Getting a random value (can be reblaced with string or a fixed int)
             os.remove(obj) # Remove the file
             count += 1 # Add a 1 to the counter after the file deletion thing has been made
-            print(f"\n---------Script--------\Removing {obj}. Deleted: {count}\n-------------------\n")
+            print(f"\n---------Script--------\nRemoving {obj}. Deleted: {count}\n-------------------\n")
 
         else: # Or, if name isn't a file, then proceed doing this
-            print(f"\n---------Script--------\Removing {obj} with its contents..\n_______________\n.")
+            print(f"\n---------Script--------\nRemoving {obj} with its contents..\n_______________\n.")
             shutil.rmtree(obj) # Using shutil (a library) we're removing the directory
             count += 1
-            print(f"\n---------Script--------\Removing {obj}. Deleted: {count}\n-------------------\n")
+            print(f"\n---------Script--------\nRemoving {obj}. Deleted: {count}\n-------------------\n")
 
 except Exception as ex: # Printing out error (just for script to be beautiful)
     print(f"\n-----Script-Error------\nOops, an error, details: {ex}\n-------------------______\n") # Printing out the error
